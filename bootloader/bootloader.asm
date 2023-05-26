7303415c6dab69fbb02c0dab9343de1241f32d0e
Deri <dominikcybulski@protonmail.com>
2023-05-26 15:25:53 +0200

    Imlements the basic bootloader

---
+++ bootloader/bootloader.asm
@@ -0,0 +1,24 @@
+[org 0x7c00]
+BIOS_WRITE equ 0x10
+WRITE_AH equ 0x0e
+
+boot:
+	cli ; no interrupts
+	cld ; all that we need to init
+	mov ax, 0x50
+;; set the buffer
+	mov es, ax
+xor bx, bx
+mov al, 2 ; read 2 sector
+mov ch, 0 ; track 0
+mov cl, 2 ; sector to read (The second sector)
+mov dh, 0 ; head number
+mov dl, 0 ; drive number
+mov ah, 0x02 ; read sectors from disk
+int 0x13 ; call the BIOS routine
+jmp 0x50:0x0 ; jump and execute the sector!
+
+jmp	$
+
+times	510-($-$$) db 0 	;marking as boot file
+db	0x55, 0xaa	
