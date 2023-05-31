f17c70f0c465d77ffe1caf2ef4bd260499c32129
Deri <dominikcybulski@protonmail.com>
2023-05-31 14:32:50 +0200

    Implements print string and clear screen functions)

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,22 +1,25 @@
+#define REG_SCREEN_CTRL 0x3D4
+#define REG_SCREEN_DATA 0x3D5
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+#define WHITE_ON_BLACK 0x0f
+
 char * getVideoAdress(int, int);
 void print(char *, int, int);
+void clearScreen();
 
 int main(){
-    //*(char*)0xb8000 = 'Q';  
-    char* video_memory = (char*)0xb8000;
     
-    //char first_char = 'A';
-    //for (int i = 0; i < 1000; i++){
-    //    video_memory[2*i] = first_char+(i % 26);
-    //}
     print("Hello, world!", 0, 0);
+    clearScreen();
     print("Hello, world!", 0, 1);
 
     return 0;
 }
 
 char * getVideoAdress(int column, int row){
-    char * toReturn = (char *)(0xb8000 + 2 * (column + row * 80));
+    char * toReturn = (char *) (VIDEO_ADDRESS + 2 * (column + row * 80));
     return toReturn;
 }
 
@@ -28,4 +31,14 @@ void print(char * string, int column, int row){
         address += 2;
     }
 
+}
+
+void clearScreen(){
+    int row = 0;
+    int col = 0;
+    for (row = 0; row < MAX_ROWS; row++){
+        for (col = 0; col < MAX_COLS; col++){
+            print(" ", col, row);
+        }
+    }
 }
